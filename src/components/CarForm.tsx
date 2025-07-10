144f526d11b4215c7e5f4ad5fa1e8598c8dd168c
boriphat12 <boriphataranyakanon@gmail.com>
2025-07-10 18:21:27 +0700

    add car form css

--- src/components/CarForm.tsx
+++ src/components/CarForm.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks"
 import { createCar, updCar } from "../features/cars/carSlice";
 import { useNavigate, useParams } from "react-router-dom";
+import './css/CarForm.css'
 
 const CarForm = () => {
     const dispatch = useAppDispatch();
