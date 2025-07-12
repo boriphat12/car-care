cd6af5966b5629f89e8223dcde8ac043dbee587c
boriphat12 <boriphataranyakanon@gmail.com>
2025-07-12 18:09:48 +0700

    add search bar in finished page

--- src/components/FinishedCars.tsx
+++ src/components/FinishedCars.tsx
@@ -1,15 +1,24 @@
 import { useAppSelector } from "../hooks";
 import './css/FinishedCars.css';
 import imgCar from '../image/car.png'
+import { useState } from "react";
 
 const FinishedCars = () => {
     const cars = useAppSelector((state) => state.car);
-    const finishedCars = cars.filter(car => car.status === "finish");
+    const [search, setSearch] = useState('')
+    const finishedCars = cars.filter(car => car.status === "finish" &&
+        car.licensePlate.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <div className="finished-cars">
             <h1>Finished Cars</h1>
             <p>Check out some of the cars we’ve recently serviced!</p>
+            <input 
+                placeholder="Search by license plate..."
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)} 
+            />
             <div className="car-grid">
                 {finishedCars.map((car) => (
                     <div className="car-card" key={car.id}>
